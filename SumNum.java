bbc2fe32b5982d51fbe591e5f841d3c1ebc1baa4
sumitgoyal4321 <sumitgoyal4321@gmail.com>
2016-03-03 21:47:56 +0530

    Added files via upload

---
+++ SumNum.java
@@ -0,0 +1,11 @@
+
+public class SumNum {
+	public static void main(String[]str)
+	{
+		int intNumber = 10;
+		float floatNumber = 15.5f;
+		int total = intNumber + (int)floatNumber;
+		System.out.println("Total = " + total);
+	}
+
+}
